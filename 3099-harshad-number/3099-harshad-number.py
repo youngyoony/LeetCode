bcd80569105f1c8f9968a5b8ae291d3a3eb38732
Young Yoon <161922625+youngyoony@users.noreply.github.com>
2024-07-01 17:29:59 +0900

    Time: 35 ms (52.71%), Space: 16.5 MB (33.76%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.py
+++ 3099-harshad-number/3099-harshad-number.py
@@ -1,17 +1,13 @@
 class Solution:
-    def sumOfTheDigitsOfHarshadNumber(self, x):
-
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        
         def sum_of_digits(n):
-            return sum(int(digit) for digit in str(n))
-        
-        digit_sum = sum_of_digits(x)
-        
-        if x % digit_sum == 0:
-            return digit_sum
-        else:
-            return -1
+            sumint = sum(int(digit) for digit in str(n))
+            return sumint
 
-# 예시 사용
-sol = Solution()
-print(sol.sumOfTheDigitsOfHarshadNumber(18))  # 출력: 9
-print(sol.sumOfTheDigitsOfHarshadNumber(23))  # 출력: -1
+        sumofdegits = sum_of_digits(x)
+
+        if x % sumofdegits == 0:
+            return sumofdegits   
+        else:
+            return -1   
