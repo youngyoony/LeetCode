777e2ad1d45326d62322f64d3235974f63cd2af6
Young Yoon <161922625+youngyoony@users.noreply.github.com>
2024-10-08 22:30:54 -0400

    Time: 59 ms (33.85%), Space: 16.5 MB (87.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,10 +1,8 @@
 class Solution:
      def isPalindrome(self, x: int) -> bool:
-        # 음수는 회문이 될 수 없으므로 바로 False 반환
         if x < 0:
             return False
         
-        # 숫자를 문자열로 변환
         S = str(x)
         i = 0
         j = len(S) - 1
